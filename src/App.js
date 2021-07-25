eb8e11b3b1783b988e001555af077fac36732b7f
PedroGrou <pedrogroug@gmail.com>
2021-07-25 11:25:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useGlobalContext } from './context';
+
+// components
+import Navbar from './Navbar';
+import CartContainer from './CartContainer';
+// items
+
+function App() {
+  const { loading } = useGlobalContext();
+
+  if (loading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
